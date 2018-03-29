cd2ca35532a52e359388934df64c8139920204e0
jiayiming <qq1220102512@outlook.com>
2018-03-29 23:57:51 +0800

    端口2000-》2001

--- bbs.py
+++ bbs.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
     config = dict(
         debug=True,
         host='0.0.0.0',
-        port=2000,
+        port=2001,
     )
     app.run(**config)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-#! /usr/bin python3
+#!/usr/bin python3
 
 import sys
 from os.path import abspath
